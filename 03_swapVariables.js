d722744671f5ef7be3ebc6199ac095c528376e2b
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-02-17 20:58:50 +0530

    javascript assignment 3
    javascript assignment 3

---
+++ 03_swapVariables.js
@@ -0,0 +1,19 @@
+var sweety="Sweety";
+var cuite="Cuite";
+console.log("before swaping:",sweety,cuite);
+var temp;
+temp=sweety;
+sweety=cuite;
+cuite=temp;
+console.log("after swaping:",sweety,cuite);
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("before swaping:",num1,num2,num3);
+var temp;
+temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("after swaping:",num1,num2,num3);
