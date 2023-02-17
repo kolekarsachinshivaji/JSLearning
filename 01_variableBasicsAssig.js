eec951495758f8dda4a6f75634152f751644661c
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-02-17 20:56:47 +0530

    javascript assignment 1
    assignments 1

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,25 @@
+var mobile=9021221516;
+var age=24;
+var weight=56;
+var fname="Sachin";
+var mname = "Shivaji";
+var lname="Kolekar"
+var ismarried=false;
+var lazy=false;
+var isgraduate=true;
+console.log("mobile:",mobile,"type:",typeof mobile);
+console.log("age:",age,"type:",typeof age);
+console.log("weight:",weight,"type:",typeof weight);
+
+console.log("fname:",fname,"type:",typeof fname);
+console.log("mnamege:",mname,"type:",typeof mname);
+console.log("lname:",lname,"type:",typeof lname);
+
+console.log("isMarried:",ismarried,"type:",typeof ismarried);
+console.log("lazy:",lazy,"type:",typeof lazy);
+console.log("isgraduate:",isgraduate,"type:",typeof isgraduate);
+
+console.log("Undefined-When we delacre variable and do not initlize any value that time variable contain default value which is undefined");
+console.log("Null-bydefault of variable is null")
+
+
