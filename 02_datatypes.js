e1655d21e0d95a530aba6b2fc70feb493d88ba0c
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-02-17 20:57:28 +0530

    javascript assignment 2
    javascript assignment 2

---
+++ 02_datatypes.js
@@ -0,0 +1,23 @@
+// console.log("sachin kolekar");
+
+
+// var city;
+// city="pune";
+// console.log(city);
+var myName = 'virat kohli';
+var age = 31;
+var weight = 82.5;
+var ismarried = true;
+var islazy = false;
+var graduation;
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+ console.log("typeOf variable gradutation:", typeof graduation);
+
+
+var typeofage =typeof age;
+console.log("type of variable age:",typeof age);
+
+var city=null;
+console.log("value of city variable is:",city);
+console.log("type of city is:",typeof city);
---
+++ 02_typeOf.js
@@ -0,0 +1,16 @@
+var bankName;
+console.log(bankName);
+var bankName='SBI Bank';
+console.log("Bank Name:",bankName,"type:",typeof bankName);
+
+var marks="90%";
+console.log("Marks:",marks,"type:",typeof marks);
+var isworking;
+console.log(isworking);
+var isworking=true;
+console.log("isworking:",isworking,"type",typeof isworking);
+
+var totalCount="Hundred and seven";
+console.log("totalcount:",totalCount,"type:",typeof totalCount);
+var totalCount=107;
+console.log("totalcount:",totalCount,"type:",typeof totalCount);
