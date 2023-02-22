9b6097f7d38a3efe8d04d002f1d6ead0c00b3bf4
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-02-22 16:40:44 +0530

    string template
    string template

---
+++ 04_stringTemplate.js
@@ -0,0 +1,15 @@
+
+
+
+// console.log("You are "champ" man....");
+console.log(`You are "champ" man`);
+
+var firstName = "Mohit";
+var lastName = "Sharma";
+var city = "pune";
+
+console.log("My first name",firstName,"Last Name",lastName,"city",city);
+
+//String Templet
+
+console.log(`My first name ${firstName} last name ${lastName } city ${city}`);
