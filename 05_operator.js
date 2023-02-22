2c495e3ef836e248f248b7d5680dd3094c92acbb
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-02-22 16:41:32 +0530

    operators in javascript
    operators in javascript

---
+++ 05_operator.js
@@ -0,0 +1,65 @@
+console.log(`====Arithmatic operator====`);
+
+var num1 = 10;
+var num2 = 2;
+var result = num1+num2;
+console.log(`Addition is ${result}`);
+
+var num1= 10;
+var num2 = 2;
+var result = num1-num2;
+console.log(`Substraction is ${result}`);
+
+var num1= 10;
+var num2 = 2;
+var result = num1*num2;
+console.log(`Multiplication is ${result}`);
+
+result = num1 ** num2;
+console.log(`Exponentional is ${result}`);
+
+// console.log(10/3);
+
+// console.log(15%3);
+
+result= num1/3;
+console.log(`Division is ${result}`);
+
+result= num1%3;
+console.log(`Reminder is ${result}`);
+
+
+
+var num=10;//compound addition
+num+=20;//num=num+20
+
+console.log(`compound addition ${num}`);
+
+console.log(`comparison operators`);
+var num3="10";
+var num4=10
+console.log(num3==10);//10==10
+console.log(num3===10);
+
+
+console.log (num3 != num4);// ! not
+
+console.log(10 > 20);// > Greater than
+
+console.log(20 < 10);// < Less than
+
+var marks = 35;
+console.log( marks >= 35); // >=Greater than equal
+
+
+
+
+
+
+
+
+
+
+
+
+
