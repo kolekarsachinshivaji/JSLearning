7ea4d8828cd3764620cbc515e0096a961c4b5a91
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-02-26 04:49:35 +0530

    what is NaN

---
+++ 05_NaN.js
@@ -0,0 +1,29 @@
+
+
+
+console.log(10 + "Hi");
+
+console.log("Good" + "Morning");
+
+
+console.log(`operation: 10-Hi ==>`,10-"Hi");//Not a Number
+
+console.log("operation : 0/0 ==>", 0/0);
+
+
+var num="10";
+
+console.log(typeof num);
+
+var num2 = +num;
+console.log(`${num2} and its type ${typeof num2}`);
+
+var myName = "Bilgates";
+
+var result = +myName;
+console.log(`${result} and its type ${typeof result}`);
+
+
+console.log(10 + true);
+
+console.log(10 - true);
