8f1188372322cd4e719e6ee76dbf0299bf631df8
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-03-01 21:10:58 +0530

    for loop
    for loop

---
+++ 06_for_loop.js
@@ -0,0 +1,21 @@
+for (let index = 1; index <=10; index++) {
+    
+    console.log(index);
+}
+
+
+console.log("table of 5")
+
+for (let index = 5; index <=50; index=index+5) {
+    
+    console.log(index);
+}
+
+console.log(`*************************`);
+
+  
+
+for (let index = 10; index >=0; index++) {
+    console.log(index);
+    
+}
