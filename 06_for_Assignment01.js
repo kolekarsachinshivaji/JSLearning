5bd46786ad6effa57008377f4f3751e9316485d5
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-03-01 21:43:32 +0530

    06_for_Assignment01
    06_for_Assignment01
    programs
    table even odd reverse no using for loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,49 @@
+ console.log(`WAP to print 5 to 15 by incrementing 1`);
+
+for (let index = 5; index <= 15 ; index++) {
+    
+    console.log(index);
+}
+
+console.log(`WAP to print numbers from 50 to 40 by decrementing by 1`);
+ 
+for (let index = 50; index >= 40  ; index--) {
+    
+    console.log(index);
+    
+}
+
+console.log(`WAP to First 15 Odd No`);
+
+for (let index = 1; index <=30; index=index+2) {
+    
+    console.log(index);
+}
+
+console.log(`WAP to First 10 Even No`);
+
+for (let index = 2; index <=20; index=index+2) {
+    
+    console.log(index);
+}
+
+console.log(`WAP to print table 5 `);
+
+for (let index = 5; index <=50; index=index+5) {
+    
+    console.log(index);
+}
+
+console.log(`WAP to print table 10 `);
+
+for (let index = 10; index <=100; index=index+10) {
+    
+    console.log(index);
+}
+
+console.log(`WAP to print table 10 in Reverse order `);
+
+for (let index = 100; index >=10; index=index-10) {
+    
+    console.log(index);
+}
