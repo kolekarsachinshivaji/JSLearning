2a85e0c1906c70dd1d85586037cbc2ef7c3d477b
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-03-05 03:30:39 +0530

    difference between var let and const keyword
    difference between var let and const keyword

---
+++ 07_variable_scope.js
@@ -0,0 +1,30 @@
+var firsrtName;
+
+firsrtName = "virat";
+
+lastName = "kohli";
+
+firsrtName = "king kohli";
+
+var lastName;
+
+console.log(lastName);
+
+
+
+let city;
+city = "delhi"
+
+//let city = "mumbai";// not allowed re declare varriable.
+const pi=3.14;
+
+//pi=3.56;
+console.log(pi);
+
+
+num=10;
+if (num==10) {
+    var greet = "Rivision is the mother of success";
+
+}
+console.log(greet);
