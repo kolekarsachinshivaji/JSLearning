5c465f91de062772d70ac308b9748e16aa8ede2c
kolekarsachinshivaji <kolekarsachinshivaji@gmail.com>
2023-03-30 14:50:29 +0530

    assignmet click event and promt
    assignmet click event and promt

--- dom/script.js
+++ dom/script.js
@@ -46,7 +46,87 @@ const elementDivProject = document.querySelector("#divProject");
 const elementProject = document.querySelector("#project");
 elementDivProject.removeChild(elementProject);
 
-// const removeChild=document.querySelector()
+console.log("removeChild=document.querySelector");
  const unorderd= document.querySelector("#unorderd");
 const list=document.querySelector("#list");
 unorderd.removeChild(list)
+
+
+
+console.log("====== Adding Element =====");
+const elementDiv = document.querySelector("#divProject");
+const elementP = document.createElement("p");
+const elementText = document.createTextNode("Team Size - 7");
+elementP.appendChild(elementText);
+elementDiv.appendChild(elementP);
+
+const elementAdress = document.querySelector("#adress")
+elementAdress.addEventListener("click",()=>{
+    console.log("Listening click event....");
+})
+
+// elementAdress.addEventListener("mouseover",()=>{
+//     console.log("mouseover....");
+// })
+
+function show(){
+    console.log("event click.........");
+}
+
+// const bodynode =document.querySelector("body");
+// const elementh=document.createElement("h3")
+// const tNode=document.createTextNode("React")
+
+// elementh.appendChild(tNode);
+// bodynode.appendChild(elementh)
+
+
+const selectbutton = document.querySelector("#myhobbies");
+selectbutton.addEventListener("click",()=>{
+    const selectDiv=document.querySelector(".div");
+    const paragrapTag=document.createElement("p");
+    const hobbie=document.createTextNode("programing")
+   selectDiv.appendChild(paragrapTag);
+   paragrapTag.appendChild(hobbie);
+   
+})
+
+selectbutton.addEventListener("click",()=>{
+    const selectDiv=document.querySelector(".div");
+    const paragrapTag=document.createElement("p");
+    const hobbie=document.createTextNode("Reading")
+   selectDiv.appendChild(paragrapTag);
+   paragrapTag.appendChild(hobbie);
+   
+})
+selectbutton.addEventListener("click",()=>{
+    const selectDiv=document.querySelector(".div");
+    const paragrapTag=document.createElement("p");
+    const hobbie=document.createTextNode("Dancing")
+   selectDiv.appendChild(paragrapTag);
+   paragrapTag.appendChild(hobbie);
+   
+})
+
+
+
+
+const isprimebutton=document.querySelector("#isprime");
+
+isprimebutton.addEventListener("click",()=>{
+
+const number = prompt("please enter the number",0)
+let c=0;
+for (let index = 1; index <=number; index++) {
+    
+    if (number%index==0) {
+        c++
+    }   
+}
+if (c==2) {
+    alert(`${number}: is prime`);
+}
+else{
+    alert(`${number}: is Not prime`);
+}
+})
